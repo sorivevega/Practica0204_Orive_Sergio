5857866822a9ad1cb2e180743db3f3a4995dff81
sorivevega <sorivevega@educacion.navarra.es>
2024-10-28 11:17:53 +0100

    Create Ejercicio01.py

---
+++ Ejercicio01.py
@@ -0,0 +1,3 @@
+#Escribir un programa que almacene las asignaturas de un curso (por ejemplo Matemáticas, Física, Química, Historia y Lengua) en una lista y la muestre por pantalla.
+list=['Matematicas', 'Fisica', 'Quimica', 'Lengua']
+print(list)
