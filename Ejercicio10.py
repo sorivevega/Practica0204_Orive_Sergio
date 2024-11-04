5f2d03d7c08754d81d881feece028f8936c0a57f
sorivevega <sorivevega@educacion.navarra.es>
2024-11-04 12:00:35 +0100

    Finalizado Ejercicio 10

---
+++ Ejercicio10.py
@@ -0,0 +1,4 @@
+#Escribir un programa que almacene en una lista los siguientes precios, 50, 75, 46, 22, 80, 65, 8, y muestre por pantalla el menor y el mayor de los precios.
+nº = ['50', '75', '46', '22', '80', '65', '08']
+print(max(nº))
+print(min(nº))
